8d599f5c3909404940f0bf2168af7a86823ef70d
stole25 <119949884+stole25@users.noreply.github.com>
2024-11-28 18:03:24 +0100

    part 1

---
+++ BookAPI/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Controllers.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext (DbContextOptions<AppDbContext> options) :
+        base (options) {}
+    public DbSet<Book> Books { get ; set ;}
+}
---
+++ BookAPI/Data/Book.cs
@@ -0,0 +1,20 @@
+namespace BookAPI.Controllers.Data;
+
+public class Book
+{
+    public int Id { get ; set ; }
+        public string Title { get ; set ;}
+        public string Description { get ; set ;}
+      //  Da li smo pročitali knjigu
+        public bool IsRead { get ; set ;}
+        //datum kada smo ju pročitali (nullable Datetime ako ju nismo pročitala datum
+        //će imati vrijednost null)
+        public DateTime? DateRead { get ; set ;}
+        //ocjena (nullable int ako ju nismo pročitali ne možemo ju ocijeniti pa će
+        //vrijednost biti null)
+    public int ? Rate { get ; set ;}
+        public string Genre { get ; set ;}
+        public string Author { get ; set ;}
+        public string CoverPictureURL { get ; set ;}
+        public DateTime DateAdded { get ; set ; }
+}
