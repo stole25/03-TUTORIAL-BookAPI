7c5ed10609d1e9e82198fe3679a6926a0c686d24
stole25 <119949884+stole25@users.noreply.github.com>
2024-11-28 19:33:41 +0100

    Bstanic

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
